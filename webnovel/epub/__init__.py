872dd5e6d4cdf03559c95f9ddba265c82f08e441
Brandy Sandrowicz <brandy@sandrowicz.org>
2023-03-15 10:33:55 -0400

    Cleaning epub.__init__

--- webnovel/epub/__init__.py
+++ webnovel/epub/__init__.py
@@ -1,3 +1,10 @@
 """Package containing all EPUB-related code."""
 
 from .data import EpubChapter, EpubMetadata
+from .pkg import EpubPackage
+
+__all__ = [
+    "EpubChapter",
+    "EpubMetadata",
+    "EpubPackage",
+]
