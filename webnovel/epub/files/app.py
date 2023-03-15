c1e3e4d268a7eedd7cc070f09f41b7c2e4975a11
Brandy Sandrowicz <brandy@sandrowicz.org>
2023-03-14 23:18:33 -0400

    fix up PyWebNovelJSON

--- webnovel/epub/files/app.py
+++ webnovel/epub/files/app.py
@@ -1,5 +1,6 @@
 """Files in the .epub Package that are internal to PyWebnovel."""
 
+import json
 from typing import TYPE_CHECKING
 
 from webnovel.epub.files import EpubFileInterface
@@ -9,6 +10,14 @@ if TYPE_CHECKING:
 
 
 class PyWebNovelJSON(EpubFileInterface):
+    """
+    A JSON file storing information about the webnovel.
+
+    This file is a place to store PyWebnovel specific information so that it can
+    be read out of existing epub files to do things like (e.g.) fetch additional
+    chapters and add them to the file.
+    """
+
     file_id: str = "pywebnovel-meta"
     filename: str = "pywebnovel.json"
     mimetype: str = "application/json"
@@ -19,3 +28,8 @@ class PyWebNovelJSON(EpubFileInterface):
 
     def __init__(self, pkg: "EpubPackage") -> None:
         self.pkg = pkg
+
+    def generate(self) -> None:
+        """Serialize the novel information into the data attribute as JSON."""
+        data = {}
+        self.data = json.dumps(data).encode("utf-8")
