1b5f95cce020845e2e0c3b005a30ad0440bde4a3
Brandy Sandrowicz <brandy@sandrowicz.org>
2023-03-15 00:09:07 -0400

    Move ncx file into webnovel.epub.files.__init__

--- webnovel/epub/files/__init__.py
+++ webnovel/epub/files/__init__.py
@@ -5,8 +5,10 @@ import pkgutil
 from typing import TYPE_CHECKING
 from xml.dom.minidom import getDOMImplementation
 
+from webnovel.xml import create_element, set_element_attributes
+
 from .base import BasicFileInterface, EpubFile, EpubFileInterface
-from .content import CoverPage, NavigationControlFile, TableOfContentsPage, TitlePage
+from .content import CoverPage, TableOfContentsPage, TitlePage
 from .images import EpubImage, EpubImages
 
 if TYPE_CHECKING:
@@ -104,3 +106,68 @@ class Stylesheet(EpubFileInterface):
     def generate(self) -> None:
         """Load the stylesheet data from embeded stylesheet."""
         self.data = pkgutil.get_data("webnovel.epub", "content/stylesheet.css")
+
+
+class NavigationControlFile(EpubFileInterface):
+    """
+    The toc.ncx file or 'Navigation Control for XML' file.
+
+    The NCX file abbreviated as a Navigation Control file for XML, usually named
+    toc.ncx. This file consists of the hierarchical table of contents for an
+    EPUB file. The specification for NCX was developed for Digital Talking Book
+    (DTB) and this file format is maintained by the DAISY Consortium and is not
+    a part of the EPUB specification. The NCX file includes a mime-type of
+    application/x-dtbncx+xml into it.
+
+    Source: https://docs.fileformat.com/ebook/ncx/
+    """
+
+    file_id: str = "ncx"
+    filename: str = "toc.ncx"
+    mimetype: str = "application/x-dtbncx+xml"
+    title: str = None
+    include_in_spine: bool = False
+    data: bytes = None
+    pkg: "EpubPackage" = None
+
+    # Skip: cover.xhtml, toc.ncx, stylesheet.css (and images)
+    files_to_ignore = ("cover", "ncx", "style", "mimetype", "container-xml", "pywebnovel-meta")
+
+    def __init__(self, pkg: "EpubPackage") -> None:
+        self.pkg = pkg
+
+    def generate(self):
+        """Generate XML Contents into data attribute."""
+        dom = getDOMImplementation().createDocument(None, "ncx", None)
+        set_element_attributes(
+            dom.documentElement,
+            {
+                "version": "2005-1",
+                "xmlns": "http://www.daisy.org/z3986/2005/ncx/",
+            },
+        )
+        head = create_element(dom, "head", parent=dom.documentElement)
+        doc_title = create_element(dom, "docTitle", parent=dom.documentElement)
+        navmap = create_element(dom, "navMap", parent=dom.documentElement)
+
+        create_element(dom, "meta", parent=head, attributes={"name": "dtb:uid", "content": self.pkg.epub_uid})
+        create_element(dom, "meta", parent=head, attributes={"name": "dtb:depth", "content": "1"})
+        create_element(dom, "meta", parent=head, attributes={"name": "dtb:totalPageCount", "content": "0"})
+        create_element(dom, "meta", parent=head, attributes={"name": "dtb:maxPageNumber", "content": "0"})
+        create_element(dom, "text", parent=doc_title, text=self.pkg.novel.title)
+
+        for index, epub_file in enumerate(self.pkg.files):
+            # TODO: There's probably a better way to just label the files that
+            #       need to be included here rather than maintaining a
+            #       blacklist.  A property off the file class or a whitelist of
+            #       classes, etc.
+            if epub_file.file_id in self.files_to_ignore:
+                continue
+            nav_point = create_element(
+                dom, "navPoint", parent=navmap, attributes={"id": epub_file.file_id, "playOrder": index}
+            )
+            nav_label = create_element(dom, "navLabel", parent=nav_point)
+            create_element(dom, "text", parent=nav_label, text=epub_file)
+            create_element(dom, "content", parent=nav_point, attributes={"src": epub_file.filename})
+
+        self.data = dom.toxml(encoding="utf-8")
