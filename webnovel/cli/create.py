12036bfe7ecf02682229d64e37fa79d4ce2c5b44
Brandy Sandrowicz <brandy@sandrowicz.org>
2023-03-22 19:42:47 -0400

    start cleaning up the cli

--- webnovel/cli/create.py
+++ webnovel/cli/create.py
@@ -5,6 +5,18 @@ import logging
 
 from webnovel import actions, turn_on_logging
 
+arg_parser = argparse.ArgumentParser(description="Create an .epub file ")
+arg_parser.add_argument("-u", "--novel-url", help="The URL of the novel.")
+arg_parser.add_argument("-f", "--filename", help='The name of the .epub file to create. Defaults to "Novel Title.epub"')
+arg_parser.add_argument(
+    "--cover-image-url",
+    help=(
+        "Provide an alternative cover image url. By default, this will be "
+        "scraped from the novel URL. If provided, this option will override "
+        "that."
+    ),
+)
+
 
 def setup_logging():
     """Initialize logging settings."""
@@ -18,20 +30,10 @@ def setup_logging():
     logger.setLevel(logging.DEBUG)
 
 
-def get_cli_options():
-    """Parse the CLI options."""
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("-u", "--novel-url", help="The URL of the novel. Only used on ")
-    parser.add_argument("-f", "--filename", help="The name of the epub file to create.")
-    parser.add_argument("--cover-image-url", help="Provide an alternative cover image url.")
-    options = parser.parse_args()
-    return options
-
-
 def run():
     """Run the command."""
     turn_on_logging()
-    options = get_cli_options()
+    options = arg_parser.parse_args()
     actions.create_epub(
         options.novel_url,
         options.filename,
