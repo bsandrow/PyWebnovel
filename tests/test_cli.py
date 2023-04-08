cc8a4bbf9b4bba7eed06920cc26eb811c108c385
Brandy Sandrowicz <brandy@sandrowicz.org>
2023-04-08 16:48:41 -0400

    Convert webnovel.cli to use click

---
+++ tests/test_cli.py
@@ -0,0 +1,70 @@
+from unittest import TestCase, mock
+
+from click.testing import CliRunner
+
+from webnovel import cli
+
+
+class SetCoverTestCase(TestCase):
+    @mock.patch("webnovel.actions.set_cover_image_for_epub")
+    def test_run(self, action_mock):
+        runner = CliRunner()
+        result = runner.invoke(cli.pywn, ["set-cover", "mybook.epub", "cover-image.tiff"])
+        self.assertEqual(result.exit_code, 0)
+        action_mock.assert_called_once_with("mybook.epub", "cover-image.tiff")
+
+
+class RebuildCommandTestCase(TestCase):
+    @mock.patch("webnovel.actions.rebuild")
+    def test_run(self, action_mock):
+        runner = CliRunner()
+        result = runner.invoke(cli.pywn, ["rebuild", "mybook.epub"])
+        self.assertEqual(result.exit_code, 0)
+        action_mock.assert_called_once_with("mybook.epub")
+
+
+class CreateCommandTestCase(TestCase):
+    @mock.patch("webnovel.actions.create_epub")
+    def test_minimum_call(self, action_mock):
+        runner = CliRunner()
+        result = runner.invoke(cli.pywn, ["create", "https://example.com/novel/1"])
+        self.assertEqual(result.exit_code, 0)
+        action_mock.assert_called_once_with(
+            "https://example.com/novel/1", None, cover_image_url=None, chapter_limit=None
+        )
+
+    @mock.patch("webnovel.actions.create_epub")
+    def test_handles_filename(self, action_mock):
+        runner = CliRunner()
+        result = runner.invoke(cli.pywn, ["create", "https://example.com/novel/1", "--filename=test.epub"])
+        self.assertEqual(result.exit_code, 0)
+        action_mock.assert_called_once_with(
+            "https://example.com/novel/1", "test.epub", cover_image_url=None, chapter_limit=None
+        )
+
+    @mock.patch("webnovel.actions.create_epub")
+    def test_handles_cover_image_url(self, action_mock):
+        runner = CliRunner()
+        result = runner.invoke(
+            cli.pywn,
+            ["create", "https://example.com/novel/1", "--cover-image=https://example.com/imgs/cover-image.png"],
+        )
+        self.assertEqual(result.exit_code, 0)
+        action_mock.assert_called_once_with(
+            "https://example.com/novel/1",
+            None,
+            cover_image_url="https://example.com/imgs/cover-image.png",
+            chapter_limit=None,
+        )
+
+    @mock.patch("webnovel.actions.create_epub")
+    def test_handles_chapter_limit(self, action_mock):
+        runner = CliRunner()
+        result = runner.invoke(cli.pywn, ["create", "https://example.com/novel/1", "--chapter-limit=40"])
+        self.assertEqual(result.exit_code, 0)
+        action_mock.assert_called_once_with(
+            "https://example.com/novel/1",
+            None,
+            cover_image_url=None,
+            chapter_limit=40,
+        )
---
+++ webnovel/cli.py
@@ -0,0 +1,96 @@
+"""The command-line interface to PyWebnovel."""
+
+import click
+
+from webnovel import actions, turn_on_logging
+
+
+class Namespace(dict):
+    """A simple wrapper around dict that allows accessing items as attributes."""
+
+    def __getattr__(self, name):
+        """Return items via __getitem__ if it's not a pre-existing attribute on self."""
+        if name in self:
+            return self[name]
+        raise AttributeError(f"'{self.__class__.__name__}' object has no attribute {name!r}")
+
+    def __setattr__(self, name, value):
+        """Allow setting of attributes to be handled via __setitem__."""
+        self[name] = value
+
+
+@click.group()
+@click.option("--debug/--no-debug", default=False)
+@click.option("--format", default="epub", help="Specify the format of the ebook. Currently only 'epub' is supported.")
+@click.pass_context
+def pywn(ctx, debug, format):
+    """
+    Create, edit and update ebooks of webnovels.
+
+    NOTE: Only ebooks created by PyWebnovel can be managed in this way as
+          application data is stored in a JSON file within the ebook itself.
+    """
+    print("--> HERE <--")
+    ctx.ensure_object(Namespace)
+    ctx.obj.debug = debug
+    ctx.obj.format = format
+
+
+@pywn.command()
+@click.argument("novel_url")
+@click.option(
+    "--filename",
+    "-f",
+    help="The filename to save the ebook as. Defaults to: {{TITLE}}.{{EXTENSION}}",
+)
+@click.option(
+    "--chapter-limit", default=None, type=int, help="Set the maximum number of chapters to build into the ebook."
+)
+@click.option(
+    "--cover-image",
+    help=(
+        "Provide an alternative cover image to override the cover image "
+        "parsed from the novel page. This should be a URL. In the future, "
+        "providing a path to an image file will also be supported."
+    ),
+)
+@click.pass_obj
+def create(opts, novel_url, filename, chapter_limit, cover_image):
+    """Create an ebook file from NOVEL_URL."""
+    turn_on_logging()
+    actions.create_epub(
+        novel_url,
+        filename,
+        cover_image_url=cover_image,
+        chapter_limit=chapter_limit,
+    )
+
+
+@pywn.command()
+@click.argument("ebook")
+@click.pass_obj
+def rebuild(opts, ebook):
+    """
+    Rebuild an existing ebook.
+
+    This is useful if the building code (e.g. the xhtml templates) have changed
+    and you want to update the ebook without building a completely new ebook
+    from scratch which would include re-downloading all chapters, images, etc.
+    """
+    turn_on_logging()
+    actions.rebuild(ebook)
+
+
+@pywn.command()
+@click.argument("ebook")
+@click.argument("cover_image")
+@click.pass_obj
+def set_cover(opts: Namespace, ebook: str, cover_image: str) -> None:
+    """
+    Set the cover image for EBOOK.
+
+    COVER_IMAGE can be a path to an image file or a URL.
+    """
+    click.echo("GOT HERE!")
+    turn_on_logging()
+    actions.set_cover_image_for_epub(ebook, cover_image)
