f5fab4a6115507db2f6bccecff3e6387228401c8
Brandy Sandrowicz <brandy@sandrowicz.org>
2023-04-09 01:13:11 -0400

    Move actions test case mixin to helpers

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,5 +1,14 @@
+from contextlib import contextmanager
 import pathlib
-from typing import Union
+import tempfile
+from typing import Iterable, Optional, Union
+from unittest import TestCase as TestCase_orig
+from zipfile import ZipFile
+
+from freezegun import freeze_time
+from requests_mock import Mocker as RequestsMocker
+
+from webnovel import data, epub
 
 TEST_DATA_DIR = pathlib.Path(__file__).parent / "data"
 
@@ -7,3 +16,162 @@ TEST_DATA_DIR = pathlib.Path(__file__).parent / "data"
 def get_test_data(filename, use_bytes: bool = False) -> Union[str, bytes]:
     with (TEST_DATA_DIR / filename).open(mode="rb" if use_bytes else "r") as fh:
         return fh.read()
+
+
+class TestCase(TestCase_orig):
+    # requests_mock: RequestsMocker
+
+    # # A mapping of URL / Path to file contents
+    # mocked_requests: dict[str, str] = None
+
+    # def setUp(self):
+    #     self.requests_mock = RequestsMocker()
+    #     self.requests_mock.start()
+    #     for url, content in (self.mocked_requests or {}).items():
+    #         kwargs = {"content": content} if isinstance(content, bytes) else {"text": content}
+    #         self.requests_mock.get(url, **kwargs)
+
+    # def tearDown(self):
+    #     self.requests_mock.stop()
+
+    @staticmethod
+    def create_epub(jpg: bytes = None, timestamp: str = "2001-01-01 12:15", chapters: list[data.Chapter] = None):
+        """Create an epub file for testing purposes."""
+        _, epubfile = tempfile.mkstemp(prefix="pywebnovel_")
+        pkg = epub.EpubPackage(
+            file_or_io=epubfile,
+            options={
+                "include_title_page": True,
+                "include_toc_page": True,
+                "include_images": True,
+            },
+            metadata={
+                "novel_url": "https://example.com/novel/creepy-story-club",
+                "site_id": "Example.com",
+                "novel_id": "creepy-story-club",
+                "title": "Creepy Story Club",
+                "status": data.NovelStatus.ONGOING.value,
+                "summary": "A\nB\nC",
+                "summary_type": epub.SummaryType.text.value,
+                "author": {"name": "John Smythe"},
+                "cover_image_url": "https://example.com/imgs/creepy-story-club.jpg",
+            },
+        )
+
+        if jpg:
+            pkg.add_image(
+                image=data.Image(url="", data=jpg, mimetype="image/jpeg", did_load=True),
+                content=jpg,
+                is_cover_image=True,
+            )
+
+        for chapter in chapters or []:
+            pkg.add_chapter(chapter)
+
+        with freeze_time(timestamp):
+            pkg.save()
+
+        return epubfile
+
+    @contextmanager
+    def assert_files_changed(
+        self,
+        epub_file: str,
+        expected_changed_files: Iterable,
+        expected_new_files: Optional[Iterable] = None,
+        expected_initial_files: Optional[Iterable] = None,
+        expected_final_files: Optional[Iterable] = None,
+    ):
+        """Assert Before/After Changes on the contents of an EPUB file."""
+
+        # NOTE: The explicit test for None when choosing to do assertions is a
+        # speicfic choice, which allows empty iterables to be passed when the
+        # expected set of files is an empty set.
+
+        expected_changed_files = set(expected_changed_files) if expected_changed_files is not None else None
+        expected_new_files = set(expected_new_files) if expected_new_files is not None else None
+        expected_initial_files = set(expected_initial_files) if expected_initial_files is not None else None
+        expected_final_files = set(expected_final_files) if expected_final_files is not None else None
+
+        with ZipFile(epub_file) as zfh:
+            pre_file_map = {fname: zfh.read(fname) for fname in zfh.namelist()}
+            pre_files = set(pre_file_map.keys())
+
+        #
+        # If expected_initial_files was provided, assert it.
+        #
+        if expected_initial_files is not None:
+            self.assertEqual(pre_files, expected_initial_files)
+
+        yield
+
+        with ZipFile(epub_file) as zfh:
+            post_file_map = {fname: zfh.read(fname) for fname in zfh.namelist()}
+            post_files = set(post_file_map.keys())
+
+        #
+        # If expected_final_files was provided, assert that the full list of
+        # files in post_file_map looks like it.
+        #
+        if expected_final_files is not None:
+            self.assertEqual(post_files, expected_final_files)
+
+        #
+        # If expected_new_files was provided, assert that these files are only
+        # present in post_files.
+        #
+        if expected_new_files is not None:
+            actual_new_files = post_files - pre_files
+            self.assertEqual(actual_new_files, expected_new_files)
+
+        common_files = pre_files & post_files
+
+        #
+        # Make sure that all files we expect to have changes were present both
+        # before _and_ after the change. expected_new_files should be used to
+        # assert files that were expected to only be present in the
+        # post_file_map.
+        #
+        self.assertEqual(
+            expected_changed_files - common_files,
+            set(),
+            (
+                f"Files that were expected to change where not present either in "
+                f"the package before or after the change: {expected_changed_files - common_files}"
+            ),
+        )
+
+        #
+        # Assert that all files expected to change did _not_ remain the same.
+        #
+        for filename in common_files - expected_changed_files:
+            self.assertEqual(
+                pre_file_map[filename],
+                post_file_map[filename],
+                (
+                    f"\n"
+                    f"File {filename} unexpectedly had changes to content.\n\n"
+                    f"==[ Before ]==\n\n"
+                    f"{pre_file_map[filename]!r}\n\n"
+                    f"==[ After  ]==\n\n"
+                    f"{post_file_map[filename]!r}\n\n"
+                    f"==============\n"
+                ),
+            )
+
+        #
+        # Assert that all files that were _not_ expected to change actually
+        # remained the same.
+        #
+        for filename in expected_changed_files:
+            self.assertNotEqual(
+                pre_file_map[filename],
+                post_file_map[filename],
+                (
+                    f"\n"
+                    f"File {filename} was expected to change, but didn't\n\n"
+                    f"==[ Contents ]==\n\n"
+                    f"{pre_file_map[filename]!r}\n\n"
+                    f"==============\n"
+                ),
+            )
--- tests/test_actions.py
+++ tests/test_actions.py
@@ -3,158 +3,20 @@ import os
 from pathlib import Path
 import tempfile
 from typing import Iterable, Optional
-from unittest import TestCase
 from zipfile import ZipFile
 
+from bs4 import BeautifulSoup
 from freezegun import freeze_time
 import requests_mock
 
 from webnovel import actions, data, epub
 
-from .helpers import get_test_data
+from .helpers import TestCase, get_test_data
 
 
-class ActionTestCaseMixin(TestCase):
-    @staticmethod
-    def create_epub(jpg: bytes = None, timestamp: str = "2001-01-01 12:15"):
-        _, epubfile = tempfile.mkstemp(prefix="pywebnovel_")
-        pkg = epub.EpubPackage(
-            file_or_io=epubfile,
-            options={
-                "include_title_page": True,
-                "include_toc_page": True,
-                "include_images": True,
-            },
-            metadata={
-                "novel_url": "https://example.com/novel/creepy-story-club",
-                "site_id": "Example.com",
-                "novel_id": "creepy-story-club",
-                "title": "Creepy Story Club",
-                "status": data.NovelStatus.ONGOING.value,
-                "summary": "A\nB\nC",
-                "summary_type": epub.SummaryType.text.value,
-                "author": {"name": "John Smythe"},
-                "cover_image_url": "https://example.com/imgs/creepy-story-club.jpg",
-            },
-        )
-
-        if jpg:
-            pkg.add_image(
-                image=data.Image(url="", data=jpg, mimetype="image/jpeg", did_load=True),
-                content=jpg,
-                is_cover_image=True,
-            )
-
-        with freeze_time(timestamp):
-            pkg.save()
-
-        return epubfile
-
-    @contextmanager
-    def assert_files_changed(
-        self,
-        epub_file: str,
-        expected_changed_files: Iterable,
-        expected_new_files: Optional[Iterable] = None,
-        expected_initial_files: Optional[Iterable] = None,
-        expected_final_files: Optional[Iterable] = None,
-    ):
-
-        # NOTE: The explicit test for None when choosing to do assertions is a
-        # speicfic choice, which allows empty iterables to be passed when the
-        # expected set of files is an empty set.
-
-        expected_changed_files = set(expected_changed_files) if expected_changed_files is not None else None
-        expected_new_files = set(expected_new_files) if expected_new_files is not None else None
-        expected_initial_files = set(expected_initial_files) if expected_initial_files is not None else None
-        expected_final_files = set(expected_final_files) if expected_final_files is not None else None
-
-        with ZipFile(epub_file) as zfh:
-            pre_file_map = {fname: zfh.read(fname) for fname in zfh.namelist()}
-            pre_files = set(pre_file_map.keys())
-
-        #
-        # If expected_initial_files was provided, assert it.
-        #
-        if expected_initial_files is not None:
-            self.assertEqual(pre_files, expected_initial_files)
-
-        yield
-
-        with ZipFile(epub_file) as zfh:
-            post_file_map = {fname: zfh.read(fname) for fname in zfh.namelist()}
-            post_files = set(post_file_map.keys())
-
-        #
-        # If expected_final_files was provided, assert that the full list of
-        # files in post_file_map looks like it.
-        #
-        if expected_final_files is not None:
-            self.assertEqual(post_files, expected_final_files)
-
-        #
-        # If expected_new_files was provided, assert that these files are only
-        # present in post_files.
-        #
-        if expected_new_files is not None:
-            actual_new_files = post_files - pre_files
-            self.assertEqual(actual_new_files, expected_new_files)
-
-        common_files = pre_files & post_files
-
-        #
-        # Make sure that all files we expect to have changes were present both
-        # before _and_ after the change. expected_new_files should be used to
-        # assert files that were expected to only be present in the
-        # post_file_map.
-        #
-        self.assertEqual(
-            expected_changed_files - common_files,
-            set(),
-            (
-                f"Files that were expected to change where not present either in "
-                f"the package before or after the change: {expected_changed_files - common_files}"
-            ),
-        )
-
-        #
-        # Assert that all files expected to change did _not_ remain the same.
-        #
-        for filename in common_files - expected_changed_files:
-            self.assertEqual(
-                pre_file_map[filename],
-                post_file_map[filename],
-                (
-                    f"\n"
-                    f"File {filename} unexpectedly had changes to content.\n\n"
-                    f"==[ Before ]==\n\n"
-                    f"{pre_file_map[filename]!r}\n\n"
-                    f"==[ After  ]==\n\n"
-                    f"{post_file_map[filename]!r}\n\n"
-                    f"==============\n"
-                ),
-            )
-
-        #
-        # Assert that all files that were _not_ expected to change actually
-        # remained the same.
-        #
-        for filename in expected_changed_files:
-            self.assertNotEqual(
-                pre_file_map[filename],
-                post_file_map[filename],
-                (
-                    f"\n"
-                    f"File {filename} was expected to change, but didn't\n\n"
-                    f"==[ Contents ]==\n\n"
-                    f"{pre_file_map[filename]!r}\n\n"
-                    f"==============\n"
-                ),
-            )
-
-
-class RebuildTestCase(ActionTestCaseMixin, TestCase):
+class RebuildTestCase(TestCase):
     jpg: bytes
+    epub: str
 
     @classmethod
     def setUpClass(cls):
@@ -164,15 +26,76 @@ class RebuildTestCase(ActionTestCaseMixin, TestCase):
         self.epub = self.create_epub(jpg=self.jpg)
 
     def tearDown(self):
-        os.unlink(self.epub_wo_cover)
-        os.unlink(self.epub_w_cover)
+        os.unlink(self.epub)
 
     def test_rebuild_action(self):
         with self.assert_files_changed(self.epub, expected_changed_files={"OEBPS/Text/title_page.xhtml"}):
             actions.rebuild(self.epub)
 
+    def test_reload_chapters(self):
+        chapters = [
+            data.Chapter(
+                url="https://wuxiaworld.site/novel/creepy-story-club/chapter-1/",
+                slug="chapter-1",
+                chapter_no=1,
+                html_content=BeautifulSoup(
+                    """
+                    <div>
+                    <p>Lorem ipsum dolor sit amet. Quo quas commodi ut quod vitae
+                    33 maiores soluta et provident nostrum. Eum nulla
+                    necessitatibus qui perspiciatis voluptate eum maxime galisum
+                    est obcaecati molestias aut labore cupiditate eum blanditiis
+                    doloremque. </p>
+                    </div>
+                    """
+                ),
+            ),
+            data.Chapter(
+                url="https://wuxiaworld.site/novel/creepy-story-club/chapter-2/",
+                slug="chapter-2",
+                chapter_no=2,
+                html_content=BeautifulSoup(
+                    """
+                    <div>
+                    <p>Lorem ipsum dolor sit amet. Quo quas commodi ut quod vitae
+                    33 maiores soluta et provident nostrum. Eum nulla
+                    necessitatibus qui perspiciatis voluptate eum maxime galisum
+                    est obcaecati molestias aut labore cupiditate eum blanditiis
+                    doloremque. </p>
+                    </div>
+                    """
+                ),
+            ),
+            data.Chapter(
+                url="https://wuxiaworld.site/novel/creepy-story-club/chapter-14/",
+                slug="chapter-14",
+                chapter_no=14,
+                html_content=BeautifulSoup(
+                    """
+                    <div>
+                    <p>Lorem ipsum dolor sit amet. Quo quas commodi ut quod vitae
+                    33 maiores soluta et provident nostrum. Eum nulla
+                    necessitatibus qui perspiciatis voluptate eum maxime galisum
+                    est obcaecati molestias aut labore cupiditate eum blanditiis
+                    doloremque. </p>
+                    </div>
+                    """
+                ),
+            ),
+        ]
 
-class SetCoverImageTestCase(ActionTestCaseMixin, TestCase):
+        pkg = epub.EpubPackage.load(self.epub)
+        for chapter in chapters:
+            pkg.add_chapter(chapter)
+        pkg.save()
+
+        with self.assert_files_changed(self.epub, expected_changed_files={"OEBPS/Text/title_page.xhtml"}):
+            actions.rebuild(
+                self.epub,
+            )
+
+
+class SetCoverImageTestCase(TestCase):
     jpg: bytes
     png: bytes
 
