73def3d91a51b5c0ae4b46352fb1eb7e3c585dcb
Brandy Sandrowicz <brandy@sandrowicz.org>
2023-11-02 14:52:34 -0400

    Exclude the base classes

--- webnovel/sites/__init__.py
+++ webnovel/sites/__init__.py
@@ -10,14 +10,14 @@ NOVEL_SCRAPERS: list[type[NovelScraperBase]] = [
     item
     for submodule in iter_submodules(__name__)
     for _, item in submodule.__dict__.items()
-    if inspect.isclass(item) and issubclass(item, NovelScraperBase)
+    if inspect.isclass(item) and issubclass(item, NovelScraperBase) and item != NovelScraperBase
 ]
 
 CHAPTER_SCRAPERS: list[type[ChapterScraperBase]] = [
     item
     for submodule in iter_submodules(__name__)
     for _, item in submodule.__dict__.items()
-    if inspect.isclass(item) and issubclass(item, ChapterScraperBase)
+    if inspect.isclass(item) and issubclass(item, ChapterScraperBase) and item != ChapterScraperBase
 ]
 
 
