2fbdb72a6beb4316ba783d96dc392fc333abad32
Brandy Sandrowicz <brandy@sandrowicz.org>
2023-11-02 14:17:24 -0400

    Use iter_submodules

--- webnovel/sites/__init__.py
+++ webnovel/sites/__init__.py
@@ -2,62 +2,22 @@
 
 import inspect
 
+from apptk.importing import iter_submodules
+
 from webnovel.scraping import ChapterScraperBase, NovelScraperBase
-from webnovel.sites import (
-    divinedaolibrary,
-    infamousscans,
-    novelbin,
-    novelcool,
-    novelnext,
-    reaperscans,
-    scribblehub,
-    skydemonorder,
-    wuxiarealm,
-    wuxiaworld_site,
-    wuxiaworldeu,
-)
-
-__all__ = [
-    "divinedaolibrary",
-    "infamousscans",
-    "novelbin",
-    "novelcool",
-    "novelnext",
-    "reaperscans",
-    "scribblehub",
-    "skydemonorder",
-    "wuxiarealm",
-    "wuxiaworldeu",
-    "wuxiaworld_site",
-    "find_scraper",
-]
-
-SITES = [
-    divinedaolibrary,
-    infamousscans,
-    novelbin,
-    novelcool,
-    novelnext,
-    reaperscans,
-    scribblehub,
-    skydemonorder,
-    wuxiaworld_site,
-    wuxiaworldeu,
-    wuxiarealm,
-]
 
 NOVEL_SCRAPERS: list[type[NovelScraperBase]] = [
     item
-    for site in SITES
-    for item in map(lambda x: getattr(site, x), dir(site))
-    if inspect.isclass(item) and issubclass(item, NovelScraperBase) and item != NovelScraperBase
+    for submodule in iter_submodules(__name__)
+    for _, item in submodule.__dict__.items()
+    if inspect.isclass(item) and issubclass(item, NovelScraperBase)
 ]
 
 CHAPTER_SCRAPERS: list[type[ChapterScraperBase]] = [
     item
-    for site in SITES
-    for item in map(lambda x: getattr(site, x), dir(site))
-    if inspect.isclass(item) and issubclass(item, ChapterScraperBase) and item != ChapterScraperBase
+    for submodule in iter_submodules(__name__)
+    for _, item in submodule.__dict__.items()
+    if inspect.isclass(item) and issubclass(item, ChapterScraperBase)
 ]
 
 
