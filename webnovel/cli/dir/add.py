c155d33fe186dcc98165f9874d3c1d71761b47cf
Brandy Sandrowicz <brandy@sandrowicz.org>
2023-11-26 12:37:14 -0500

    Add Event Handlers for 'pywn dir add' Command

--- webnovel/cli/dir/add.py
+++ webnovel/cli/dir/add.py
@@ -1,7 +1,10 @@
 """Command: dir add."""
 
+from pathlib import Path
+
 import click
 
+from webnovel import events
 from webnovel.actions import App
 
 from ..base import pass_app
@@ -12,4 +15,19 @@ from ..base import pass_app
 @pass_app
 def add(app: App, epub_or_url: str) -> None:
     """Add a webnovel to directory."""
+    events.register(
+        event=events.Event.EBOOK_CREATE_START,
+        callback=lambda ctx: click.echo(f"Creating ebook for: {ctx.novel_url}"),
+    )
+    events.register(
+        event=events.Event.EBOOK_CREATE_END,
+        callback=lambda ctx: click.echo(f"Ebook created: {Path(ctx.ebook.filename).name}"),
+    )
+    events.register(event=events.Event.PROCESS_CHAPTER_BATCH_START, callback=handle_chapter_batch)
     app.dir_add(app.settings.directory_options.directory, epub_or_url)
+
+
+def handle_chapter_batch(ctx: events.Context) -> None:
+    """Handle the PROCESS_CHAPTER_BATCH_START event."""
+    suffix = f" [batch: {ctx.batch_no}/{ctx.total_batches}]" if ctx.total_batches > 1 else ""
+    click.secho(f" » Scraping {ctx.batch_size} chapter(s)" + suffix, fg="magenta")
