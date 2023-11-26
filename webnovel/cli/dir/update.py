8762c7d4245fa5205e4dbcd6c5b367c63e4062a9
Brandy Sandrowicz <brandy@sandrowicz.org>
2023-11-26 11:07:08 -0500

    Change up "dir update" command display

--- webnovel/cli/dir/update.py
+++ webnovel/cli/dir/update.py
@@ -26,13 +26,11 @@ def update(app: App) -> None:
     )
     events.register(
         event=events.Event.WEBNOVEL_UPDATE_NO_NEW_CHAPTERS,
-        callback=lambda ctx: click.secho(
-            f"\r{ctx.path.name} [total: {ctx.total} new: {ctx.new}]", dim=True, fg="green", nl=False
-        ),
+        callback=lambda ctx: click.secho(f"\r{ctx.path.name} [no updates]", fg="black", bold=True, nl=False),
     )
     events.register(
         event=events.Event.WEBNOVEL_UPDATE_NEW_CHAPTER_COUNT,
-        callback=lambda ctx: click.secho(f"\r{ctx.path.name} [total: {ctx.total} new: {ctx.new}]...", fg="green"),
+        callback=lambda ctx: click.secho(f"\r{ctx.path.name} [New Chapters: {ctx.new}]...", fg="green"),
     )
     events.register(event=events.Event.PROCESS_CHAPTER_BATCH_START, callback=handle_chapter_batch)
     events.register(event=events.Event.WEBNOVEL_DIR_NOVEL_UPDATE_END, callback=lambda ctx: click.echo("", nl=True))
@@ -41,8 +39,6 @@ def update(app: App) -> None:
 
 def handle_chapter_batch(ctx: events.Context) -> None:
     """Handle the PROCESS_CHAPTER_BATCH_START event."""
-    # msg = f" -> Fetching {ctx.batch_size} chapter(s) (batch #{ctx.batch_no}): "
-    click.secho(
-        f" » Scraping {ctx.batch_size} chapters (batch {ctx.batch_no} of {ctx.total_batches})",
-        fg="magenta",
-    )
+    suffix = " [batch: {ctx.batch_no}/{ctx.total_batches}]" if ctx.total_batches > 1 else ""
+    is_last_batch = ctx.batch_no != ctx.total_batches
+    click.secho(f" » Scraping {ctx.batch_size} chapter(s)" + suffix, fg="magenta", nl=is_last_batch)
